0c9d6a0fcc1d42134598983b29c2df4fd7840fcc
Florian Pradines <florian.pradines@gmail.com>
2018-02-14 17:35:06 +0100

    Fix for random generation

--- Skeerel/Util/Random.php
+++ Skeerel/Util/Random.php
@@ -11,12 +11,14 @@ use Skeerel\Exception\IllegalArgumentException;
 class Random {
 
     public static function token($length = 40){
-        if(!is_int($length) || intval($length) <= 8 || intval()) {
+        if(!is_int($length) || intval($length) <= 8) {
             throw new IllegalArgumentException("length must be an integer value > 8");
         }
 
         if (function_exists('random_bytes')) {
-            return bin2hex(random_bytes($length));
+            try {
+                return bin2hex(random_bytes($length));
+            } catch(\Exception $ignore) {}
         }
 
         if (function_exists('openssl_random_pseudo_bytes')) {
